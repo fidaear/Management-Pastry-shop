901b4f366190cb1d521b9b5359d83b8f4d1e2716
fidaear <fidaeariyan2004x@gmail.com>
2025-12-19 05:47:35 +0100

    Initial commit and main branch

---
+++ src/main/java/com/pastryshop/pastry_backend/PastryShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pastryshop.pastry_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastryShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastryShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pastryshop/pastry_backend/PastryShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pastryshop.pastry_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastryShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
